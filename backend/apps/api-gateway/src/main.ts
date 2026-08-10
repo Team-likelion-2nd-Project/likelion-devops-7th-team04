ff3cc2b3e0b4e708b6750392b33af555a632fd68
rbsxo135 <55007502+rbsxo135@users.noreply.github.com>
2026-08-10 17:27:16 +0900

    feat: NestJS CORS (#128)

--- backend/apps/api-gateway/src/main.ts
+++ backend/apps/api-gateway/src/main.ts
@@ -3,6 +3,11 @@ import { ApiGatewayModule } from './api-gateway.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(ApiGatewayModule);
-  await app.listen(process.env.port ?? 3000);
+  
+  app.enableCors({
+    origin: 'http://localhost:5173',
+  });
+
+  await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
